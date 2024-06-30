300aa7bb9fed57f5da66247eb8fab9b4614a85c3
Patrick <celtiberi@protonmail.com>
2024-06-30 12:47:12 -0500

    allow the url

--- weather-api/index.js
+++ weather-api/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: ['http://207.5.194.71:3000', 'http://localhost:3000', 'http://127.0.0.1:3000'], // Add any other origins you need
+  origin: ['http://207.5.194.71:3000', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://channel-16.com:3000', 'https://channel-16.com:3000', 'http://channel-16.com', 'https://channel-16.com'], // Add any other origins you need
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
